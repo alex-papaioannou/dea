f7d46229819af43d2a5a5eeed3b8bca41fd97c8a
octareno <octareno@users.noreply.github.com>
2017-07-10 23:37:42 +0700

    Delete hasil.php

--- process/simplex/hasil.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	session_start();
-	include "../proses/config/koneksi.php";
-	
-	
-	mysql_query("INSERT INTO tb_perhitungan_efisiensi(hasil_perhitungan,id_dmu,id_user) VALUES('$_POST[hasil]','$_POST[id_dmu]','$_SESSION[id_user]')");
-?>
